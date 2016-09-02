c8c9df379a826f9631378bde52bb31307733a00f
Zhao Zhengrui <zhezhao@gemalto.com>
2016-09-02 17:46:43 +0200

    configure

---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+require('argv-set-env')();
+var here = require('path-here');
+var packageJson = require('./package.json');
+var _ = require('lodash');
+var webpack = require('webpack');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = getConfig();
+
+function getConfig() {
+    var config = getCommonConfig();
+
+    switch (process.env.NODE_ENV) {
+        case 'development':
+            config = _.merge(config, getDevConfig());
+            break;
+        case 'production':
+            config = _.merge(config, getProdConfig());
+            break;
+        case 'test':
+            config = _.merge(config, getTestConfig());
+            break;
+        default:
+            throw new Error(`NODE_ENV not equal to development, production, or test. It is equal to ${process.env.NODE_ENV}`);
+    }
+    return config;
+}
+
+function getCommonConfig() {
+    return {
+        context: here('app'),
+        entry: './js/index.js',
+        output: {
+            libraryTarget: 'umd',
+            library: 'jrf-editor'
+        },
+        stats: {
+            colors: true,
+            reasons: true
+        },
+        externals: {
+            angular: 'angular'
+        }
+    };
+}
+
+function getDevConfig() {
+    return {
+        output: {
+            filename: 'dist/jrf.js'
+        },
+        module: {
+            loaders: [
+                getJavaScriptLoader(),
+                getHtmlLoader(),
+            ]
+        },
+        plugins: getCommonPlugins()
+    };
+}
+
+function getProdConfig() {
+    return {
+        output: {
+            filename: 'dist/jrf.min.js'
+        },
+        devtool: 'source-map',
+        module: {
+            loaders: [
+                getJavaScriptLoader(),
+                getHtmlLoader()
+            ]
+        },
+        plugins: _.union(getCommonPlugins(), [
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new webpack.optimize.AggressiveMergingPlugin(),
+            new webpack.optimize.UglifyJsPlugin()
+        ])
+    };
+}
+
+function getJavaScriptLoader() {
+    return {
+        test: /\.js$/,
+        loaders: ['ng-annotate', 'babel?presets[]=es2015'],
+        exclude: /node_modules/,
+    };
+}
+
+function getHtmlLoader() {
+    return {
+        test: /\.html$/,
+        loaders: ['raw'],
+        exclude: /node_modules/
+    };
+}
+
+function getCommonPlugins() {
+    return _.filter([
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            VERSION: JSON.stringify(packageJson.version)
+        }),
+        process.env.CI ? undefined : new WebpackNotifierPlugin({
+            title: 'jrf',
+        })
+    ]);
+}
